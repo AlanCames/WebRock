09fcd579d7921fea07c74b73980316b0cc533230
AlanCames <87043756+AlanCames@users.noreply.github.com>
2021-09-29 18:29:56 -0300

    Add files via upload

---
+++ Reproductor/js/music-list.js
@@ -0,0 +1,74 @@
+let allMusic = [
+    {
+        name: 'Rape me',
+        artist: 'Nirvana',
+        img: 'Nirvana',
+        src: 'Rape me'
+    },
+    {
+        name: 'Come As You Are Unplugged',
+        artist: 'Nirvana',
+        img: 'Nirvana',
+        src: 'Come As You Are Unplugged'
+    },
+    {
+        name: 'Californication',
+        artist: 'Red Hot Chili Peppers',
+        img: 'RHP',
+        src: 'Red Hot Chili Peppers  Californication'
+    },
+    {
+        name: 'Under the bridge',
+        artist: 'Red Hot Chili Peppers',
+        img: 'RHP',
+        src: 'Under the bridge RHP'
+    },
+    {
+        name: 'Losing My Religion',
+        artist: 'R.E.M',
+        img: 'R.E.M',
+        src: 'REM  Losing My Religion'
+    },
+    {
+        name: 'Everybody Hurts',
+        artist: 'R.E.M',
+        img: 'R.E.M',
+        src: 'REM Everybody Hurts'
+    },
+    {
+        name: 'Let It Be',
+        artist: 'The Beatles',
+        img: 'TheBeatles',
+        src: 'Let It Be  The Beatles'
+    },
+    {
+        name: 'Love Me Do',
+        artist: 'The Beatles',
+        img: 'TheBeatles',
+        src: 'The Beatles  Love Me Do'
+    },
+    {
+        name: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        img: 'Queen',
+        src: 'Bohemian Rhapsody  Queen'
+    },
+    {
+        name: 'Love of my life',
+        artist: 'Queen',
+        img: 'Queen',
+        src: 'Love of my life  Queen'
+    },
+    {
+        name: 'Wonderwall',
+        artist: 'Oasis',
+        img: 'Oasis',
+        src: 'Oasis  Wonderwall'
+    },
+    {
+        name: 'Dont Look Back In Anger',
+        artist: 'Oasis',
+        img: 'Oasis',
+        src: 'Oasis  Dont Look Back In Anger'
+    }
+]
